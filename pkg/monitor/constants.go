755d0dce2805919edf63bb56048ce400829f6148
fridalif <fridalif2@gmail.com>
2026-07-11 20:08:47 +0300

    feat: created constants for jorunal

---
+++ pkg/monitor/constants.go
@@ -0,0 +1,12 @@
+package monitor
+
+var (
+	journalEmerge  = 0
+	journalAlert   = 1
+	journalCrit    = 2
+	journalErr     = 3
+	journalWarning = 4
+	journalNotice  = 5
+	journalInfo    = 6
+	journalDebug   = 7
+)
