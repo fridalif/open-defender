89455af6aa00e11c4059c55608cc9fa3f7e5a519
fridalif <fridalif2@gmail.com>
2026-07-11 17:57:17 +0300

    feat: created app struct and methods

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+	"open-defender/pkg/app"
+)
+
+func main() {
+	app := app.New()
+	err := app.Initialize()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = app.Run()
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
---
+++ pkg/app/app.go
@@ -0,0 +1,43 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"open-defender/pkg/config"
+	"path"
+)
+
+type App interface {
+	Initialize() error
+	Run() error
+}
+
+type app struct {
+	ctx        context.Context
+	cancel     context.CancelFunc
+	cfg        *config.Config
+	configPath string
+}
+
+func New() App {
+	ctx, cancel := context.WithCancel(context.Background())
+	return &app{
+		ctx:        ctx,
+		cancel:     cancel,
+		configPath: path.Join("", "etc", "open-defender", "config.yaml"),
+		cfg:        &config.Config{},
+	}
+}
+
+func (a *app) Initialize() error {
+	cfg := config.New()
+	err := cfg.LoadConfig(a.configPath)
+	if err != nil {
+		return fmt.Errorf("app.Initialize() -> %w", err)
+	}
+	return nil
+}
+
+func (a *app) Run() error {
+	return nil
+}
