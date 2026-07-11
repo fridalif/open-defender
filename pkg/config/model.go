e5d20a55482c85e4d76e98aedf30e33489933d4a
fridalif <fridalif2@gmail.com>
2026-07-11 13:36:02 +0300

    feat: created config for ssh monitor

--- pkg/config/model.go
+++ pkg/config/model.go
@@ -1,1 +1,14 @@
 package config
+
+type BaseFields struct {
+	Mode          string `json:"mode"` // disabled, logger, blocker
+	LogPath       string `yaml:"log_path"`
+	Tries         uint64 `yaml:"tries"`
+	WindowSeconds uint64 `yaml:"window_seconds"`
+	BanSeconds    uint64 `yaml:"ban_seconds"`
+	Pattern       uint64 `yaml:"pattern"`
+}
+
+type SSHMonitorConfig struct {
+	BaseFields
+}
