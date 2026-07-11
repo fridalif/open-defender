ebb0bc99fd8ac2f12bd9e581355bc3efe39c9575
fridalif <fridalif2@gmail.com>
2026-07-11 18:04:10 +0300

    feat: checking euid

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"log"
 	"open-defender/pkg/app"
+	"os"
 )
 
 func main() {
+	if os.Geteuid() != 0 {
+		log.Fatalln("failed to run programm: you must be a superuser")
+	}
 	app := app.New()
 	err := app.Initialize()
 	if err != nil {
