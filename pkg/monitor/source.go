39397434199707fc25158293a907cc319f86c3be
fridalif <fridalif2@gmail.com>
2026-07-12 14:25:38 +0300

    feat: created function connectToSyslog

--- pkg/monitor/source.go
+++ pkg/monitor/source.go
@@ -4,12 +4,14 @@ import (
 	"bufio"
 	"context"
 	"io"
+	"os"
 	"time"
 
 	"github.com/coreos/go-systemd/v22/sdjournal"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/pkg/stdcopy"
+	"github.com/fsnotify/fsnotify"
 )
 
 func connectToDocker(ctx context.Context, containerName string, logsChan chan<- string) error {
@@ -98,6 +100,77 @@ func connectToJournal(ctx context.Context, unitName string, logsChan chan<- stri
 	}
 }
 
-func connectToSyslog(ctx context.Context, logPath string, logsChan chan<- string) error {
+func connectToSyslog(ctx context.Context, path string, logsChan chan<- string) error {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return err
+	}
+	defer watcher.Close()
 
+	if err := watcher.Add(path); err != nil {
+		return err
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	if _, err := file.Seek(0, io.SeekEnd); err != nil {
+		return err
+	}
+
+	reader := bufio.NewReader(file)
+
+	readNewLines := func() error {
+		for {
+			line, err := reader.ReadString('\n')
+			if len(line) > 0 {
+				select {
+				case logsChan <- line:
+				case <-ctx.Done():
+					return ctx.Err()
+				}
+			}
+			if err != nil {
+				return nil
+			}
+		}
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+
+		case event, ok := <-watcher.Events:
+			if !ok {
+				return nil
+			}
+			if event.Has(fsnotify.Write) {
+				if err := readNewLines(); err != nil {
+					return err
+				}
+			}
+			if event.Has(fsnotify.Rename) || event.Has(fsnotify.Remove) {
+				file.Close()
+				newFile, err := os.Open(path)
+				if err != nil {
+					return err
+				}
+				file = newFile
+				reader = bufio.NewReader(file)
+				if err := watcher.Add(path); err != nil {
+					return err
+				}
+			}
+
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				return nil
+			}
+			return err
+		}
+	}
 }
