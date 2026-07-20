c2fddefd43e1ca7d9ad87dbf763320075267ff1d
fridalif <fridalif2@gmail.com>
2026-07-20 12:41:37 +0300

    feat: created runtime iface

--- pkg/ebpfmonitors/runtime.go
+++ pkg/ebpfmonitors/runtime.go
@@ -1,4 +1,22 @@
 package ebpfmonitors
 
-type EbpfMonitorRuntime struct {
+import (
+	"context"
+	"open-defender/pkg/config"
+)
+
+type ebpfMonitorRuntime struct {
+	ebpfConfig config.EbpfConfig
+	ctx        context.Context
+	cancel     context.CancelFunc
+}
+
+type EbpfMonitorRuntime interface{}
+
+func New(ctx context.Context, cancel context.CancelFunc, ebpfConfig config.EbpfConfig) EbpfMonitorRuntime {
+	return ebpfMonitorRuntime{
+		ctx:        ctx,
+		cancel:     cancel,
+		ebpfConfig: ebpfConfig,
+	}
 }
